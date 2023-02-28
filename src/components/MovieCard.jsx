a4aea74de1a42bc5c381bd54aff2f5e967daa68c
Deepak <sumitmanni22@gmail.com>
2023-02-28 16:53:12 +0530

    changed card size

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 //redux
 //imgUrl
 import { imgPaths } from "../api";
@@ -7,18 +6,15 @@ const MovieCard = ({ movie }) => {
 
   //states
   return (
-    <motion.div className="movie-card">
-      <motion.div className="movie-poster">
+    <div className="movie-card">
+      <div className="movie-poster">
         <img src={`${imgPaths.medium}${movie.poster_path}`} alt={movie.title} />
-      </motion.div>
+      </div>
       <div className="movie-info">
         <h4>{movie.title}</h4>
         <h5>{movie.release_date}</h5>
       </div>
-      <div className="rating">
-        <p>{movie.vote_average}</p>
-      </div>
-    </motion.div>
+    </div>
   );
 };
 
---
+++ src/components/SearchMovie.jsx
@@ -0,0 +1,9 @@
+const SearchMovie = () => {
+  return (
+    <div>
+      <h2>Welcomone</h2>
+    </div>
+  );
+};
+
+export default SearchMovie;
